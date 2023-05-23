3388d1160651f4ba5b930f750f83edaddc39de12
gauravkale2799 <gauravkale2799@gmail.com>
2023-05-23 22:31:26 +0200

    Intialization to the new Repo

---
+++ src/app/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from  "@/app/styles/navbar.module.css"
+import Link from "next/link";
+import Image from "next/image";
+const Header = () => {
+    return (
+        <header className={styles.main_header}>
+            <div className={styles.navbar_brand}>
+                < Link href="/">
+                    <Image src = "/logo1.png" alt= "my logo" width={150} height={40}/>
+                </Link>
+
+            </div>
+            <
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/app/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "@/app/styles/navbar.module.css"
+const Nav = () => {
+    return (
+        <nav className={styles.navbar}>
+        <div className={}>
+            <ul className={styles.navbarList}></ul>
+        </div>
+        </nav>
+    );
+};
+
+export default Nav;
