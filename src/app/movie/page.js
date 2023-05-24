9bc539afdda5eda636bf9dcb1209e3dd5329098f
gauravkale2799 <gauravkale2799@gmail.com>
2023-05-24 10:45:44 +0200

    Fetching API Key

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Link from "next/link";
 
-const Movie = () => {
+const Movie = async () => {
+    const url = process.env.RAPID_KEY;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3f158886aamsh87c949e8f320f48p10440fjsna679527085f5',
+            'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+        }
+    };
+
+
+    const res = await fetch(url, options);
+    const data =await res.json();
+    console.log()
     return (
         <>
          <h1>This page is of movie</h1>
+            <Link href="/movie/hgjahg">
+                go to
+            </Link>
         </>
     );
 };
