e638e29998307005b7b7ced0c20d2fc82289cfc6
gauravkale2799 <gauravkale2799@gmail.com>
2023-05-24 10:21:56 +0200

    Dynamic Route created

--- src/app/components/Herosection.js
+++ src/app/components/Herosection.js
@@ -2,7 +2,7 @@ import heroStyles from "@/app/styles/herosection.module.css"
 import styles from "@/app/styles/common.module.css"
 import Image from "next/image";
 import Link from "next/link";
-import { Mulish } from 'next/font/google';
+import {Mulish} from 'next/font/google';
 
 const mulish = Mulish({
     weight: ['300', '400', '500', '600', '700', '800', '900'],
